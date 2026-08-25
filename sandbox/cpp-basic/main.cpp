333cb8bbfff1d42024180c9a5667bba3656bcf17
silkair <silkair1234@gmail.com>
2026-08-25 21:53:57 +0900

    feat(sandbox) : 클래스와 소멸자 - 스코프에 따른 소멸 시점 확인

--- sandbox/cpp-basic/main.cpp
+++ sandbox/cpp-basic/main.cpp
@@ -1,30 +1,59 @@
 #include <iostream>
 
+class Tick {
+public:
+	Tick() {
+		std::cout << "Tick create\n";
+	}
+
+	~Tick() {
+		std::cout << "Tick delete\n";
+	}
+
+};
+
 int main() {
 
-	int x = 42;
-	int* p = &x;
+    std::cout << "--- start ---\n";
 
-	std::cout << x << std::endl;
-	std::cout << &x << std::endl;
-	std::cout << p << std::endl;
-	std::cout << *p << std::endl;
+    Tick t;
 
-	*p = 99;
+    std::cout << "---end ---\n";
 
-	std::cout << x << std::endl;
+    {
+        Tick t2;
+    }
 
+    std::cout << "--- after block ---\n";
 
-
-	int* q = new int(7);
-	std::cout << *q << std::endl;
-
-	*q = 123;
-	std::cout << *q << std::endl;
-
-	delete q;
-	q = nullptr;
-
-	return 0;
+    return 0;
 }
+
+//int main() {
+//
+//	int x = 42;
+//	int* p = &x;
+//
+//	std::cout << x << std::endl;
+//	std::cout << &x << std::endl;
+//	std::cout << p << std::endl;
+//	std::cout << *p << std::endl;
+//
+//	*p = 99;
+//
+//	std::cout << x << std::endl;
+//
+//
+//
+//	int* q = new int(7);
+//	std::cout << *q << std::endl;
+//
+//	*q = 123;
+//	std::cout << *q << std::endl;
+//
+//	delete q;
+//	q = nullptr;
+//
+//	return 0;
+//}
 
