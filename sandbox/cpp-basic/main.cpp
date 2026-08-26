d78c6150bb9de346ff8cac1e14b3ac598521da07
silkair <silkair1234@gmail.com>
2026-08-26 13:07:06 +0900

    feat(sandbox) : 클래스에 멤버 변수와 멤버 함수 추가

--- sandbox/cpp-basic/main.cpp
+++ sandbox/cpp-basic/main.cpp
@@ -1,31 +1,118 @@
 #include <iostream>
+#include <string>
 
 class Tick {
 public:
-	Tick() {
-		std::cout << "Tick create\n";
-	}
+    Tick(std::string time, int price, int qty) {
+        time_ = time;
+        price_ = price;
+        qty_ = qty;        
+        std::cout << "Tick create " <<"\n";
+    }
 
-	~Tick() {
-		std::cout << "Tick delete\n";
-	}
+    ~Tick() {
+        std::cout << "Tick delete\n";
+    }
 
+    void print() {
+        std::cout << time_ << " | " << price_ << " | " << qty_ << std::endl;
+    }
+
+private:
+    std::string time_;
+    int price_;
+    int qty_;
 };
 
 int main() {
-
-    std::cout << "--- before ---\n";
-
-    {
-        Tick* p = new Tick();
-		delete p;
-
-    }
-    std::cout << "--- after ---\n";
-
+    Tick t("09:01:23", 71500, 12);
+    t.print();
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//class Tick {
+//public:
+//    Tick(std::string time, int price, int qty) {
+//        time_ = time;
+//        price_ = price;
+//        qty_ = qty;
+//
+//        std::cout << "Tick create\n";
+//    }
+//
+//
+//    ~Tick() {
+//    	std::cout << "Tick delete\n";
+//    }
+//
+//    void print() {
+//        std::cout << time_ << " | " << price_ << " | " << qty_ << std::endl;
+//    }
+//
+//private:
+//    std::string time_;
+//    int price_;
+//    int qty_;
+//};
+//
+//int main() {
+//    std::cout << "--- start ---\n";
+//
+//    // 1. 스택(Stack) 메모리에 객체 생성
+//    Tick t("09:01:23", 71500, 12);
+//    t.print();
+//
+//    // 2. 힙(Heap) 메모리에 동적 객체 생성
+//    Tick* p = new Tick("09:01:24", 71600, 5);
+//    p->print(); // (*p).print() 와 동일
+//
+//    std::cout << "--- end ---\n";
+//
+//    // 지시문에 따라 delete p; 를 의도적으로 생략함
+//    return 0;
+//}
+
+//class Tick {
+//public:
+//	Tick() {
+//		std::cout << "Tick create\n";
+//	}
+//
+//	~Tick() {
+//		std::cout << "Tick delete\n";
+//	}
+//};
+//
+//int main() {
+//
+//    std::cout << "--- before ---\n";
+//
+//    {
+//        Tick* p = new Tick();
+//		delete p;
+//
+//    }
+//    std::cout << "--- after ---\n";
+//
+//
+//    return 0;
+//}
 
 //int main() {
 //
